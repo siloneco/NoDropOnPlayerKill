60f2f4fcc60521571ecac135c2d2e7a8e8bbab6c
siloneco <siloneco4646@gmail.com>
2020-04-29 13:52:21 +0900

    [HotFix] 1.15で運用した場合、アイテムが増殖するバグを修正

--- src/main/java/com/github/siloneco/nodrop/NoDropOnPlayerKill.java
+++ src/main/java/com/github/siloneco/nodrop/NoDropOnPlayerKill.java
@@ -3,6 +3,7 @@ package com.github.siloneco.nodrop;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,11 +21,12 @@ public class NoDropOnPlayerKill extends JavaPlugin implements Listener {
         Bukkit.getLogger().info(getName() + " disabled.");
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerDeath(PlayerDeathEvent e) {
         Player p = e.getEntity();
 
         if (p.getKiller() != null) {
+            e.getDrops().clear();
             e.setKeepInventory(true);
         }
     }
